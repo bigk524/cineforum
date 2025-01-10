0c37948aa9a8eff37a181e51482ad40b6a6dc936
Astra López <astra.lopez.zagal@gmail.com>
2025-01-10 12:15:08 -0300

    Borré todas las páginas menos home, login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,71 +18,7 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'registro',
-    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
-  },
-  {
-    path: 'peliculas',
-    loadChildren: () => import('./pages/peliculas/peliculas.module').then( m => m.PeliculasPageModule)
-  },
-  {
-    path: 'infopeli',
-    loadChildren: () => import('./pages/infopeli/infopeli.module').then( m => m.InfopeliPageModule)
-  },
-  {
-    path: 'foropeli',
-    loadChildren: () => import('./pages/foropeli/foropeli.module').then( m => m.ForopeliPageModule)
-  },
-  {
-    path: 'perfilusuario',
-    loadChildren: () => import('./pages/perfilusuario/perfilusuario.module').then( m => m.PerfilusuarioPageModule)
-  },
-  {
-    path: 'paggeneros',
-    loadChildren: () => import('./pages/paggeneros/paggeneros.module').then( m => m.PaggenerosPageModule)
-  },
-  {
-    path: 'pelistendencias',
-    loadChildren: () => import('./pages/pelistendencias/pelistendencias.module').then( m => m.PelistendenciasPageModule)
-  },
-  {
-    path: 'crearpublicacion',
-    loadChildren: () => import('./pages/crearpublicacion/crearpublicacion.module').then( m => m.CrearpublicacionPageModule)
-  },
-  {
-    path: 'modificarpublicacion',
-    loadChildren: () => import('./pages/modificarpublicacion/modificarpublicacion.module').then( m => m.ModificarpublicacionPageModule)
-  },
-  {
-    path: 'recuperarcontra',
-    loadChildren: () => import('./pages/recuperarcontra/recuperarcontra.module').then( m => m.RecuperarcontraPageModule)
-  },
-  {
-    path: 'cambiarcontra',
-    loadChildren: () => import('./pages/cambiarcontra/cambiarcontra.module').then( m => m.CambiarcontraPageModule)
-  },
-  {
-    path: 'banusuarios',
-    loadChildren: () => import('./pages/banusuarios/banusuarios.module').then( m => m.BanusuariosPageModule)
-  },
-  {
-    path: 'editarperfilusuario',
-    loadChildren: () => import('./pages/editarperfilusuario/editarperfilusuario.module').then( m => m.EditarperfilusuarioPageModule)
-  },
-  {
-    path: 'nosotros',
-    loadChildren: () => import('./pages/nosotros/nosotros.module').then( m => m.NosotrosPageModule)
-  },
-  {
-    path: 'deletepublicaciones',
-    loadChildren: () => import('./pages/deletepublicaciones/deletepublicaciones.module').then( m => m.DeletepublicacionesPageModule)
-  },
-  {
-    path: 'verelmotivodeban',
-    loadChildren: () => import('./pages/verelmotivodeban/verelmotivodeban.module').then( m => m.VerelmotivodebanPageModule)
-  },
+  }
 ];
 
 @NgModule({
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone: false
-
+  standalone: false,
 })
 export class LoginPage implements OnInit {
+  user: string = '';
+  password: string = '';
+  
+  constructor(
+    private alertController: AlertController,
+    private toastController: ToastController,
+  ) { }
 
-  constructor() { }
+  async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['Ok']
+    });
+
+    await alert.present();
+  }
+
+  async presentToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 1500,
+      position: 'bottom'
+    });
+
+    await toast.present();
+  }
+
+  validarCampos() {
+    if (this.user == 'José' && this.password == '123') {
+      this.presentAlert("Bienvenido", "Bienvenido, José!.");
+    } else {
+      this.presentToast("Credenciales inválidas");
+    }
+  }
+
+  limpiarCampos() {
+    this.user = '';
+    this.password = '';
+  }
 
   ngOnInit() {
   }
